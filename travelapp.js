bbb8ce2a0b435f7fc7611cb64e148e419196c26b
naderscode <naderscode@gmail.com>
2018-12-26 19:59:51 -0500

    create a default layouts file

--- travelapp.js
+++ travelapp.js
@@ -1,5 +1,10 @@
 var express = require('express');
 
+//setup handlebars view engine
+var handlebars = require('express-handlebars').create({defaultLayout: 'main'});
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
