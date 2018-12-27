fe8913e4f1be4727ab02255652153cda14b47575
naderscode <naderscode@gmail.com>
2018-12-26 20:51:33 -0500

    fix errors

--- travelapp.js
+++ travelapp.js
@@ -1,12 +1,13 @@
 var express = require('express');
 
+var app = express();
+
 /// set up handlebars view engine
 var handlebars = require('express-handlebars')
 	.create({ defaultLayout:'main' });
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
-var app = express();
 
 app.set('port', process.env.PORT || 3000);
 
